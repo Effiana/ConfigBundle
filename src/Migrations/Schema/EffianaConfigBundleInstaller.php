051e6500dbc7f4ce31615acb07d76bb5e56be91b
Dominik Łabudziński <dominik.labudzinski@brandoriented.pl>
2019-01-16 11:20:05 +0100

    Initial

---
+++ src/Migrations/Schema/EffianaConfigBundleInstaller.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of the Effiana package.
+ *
+ * (c) Effiana, LTD
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Dominik Labudzinski <dominik@labudzinski.com>
+ */
+declare(strict_types=0);
+
+namespace Migrations\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Effiana\MigrationBundle\Migration\Column;
+use Effiana\MigrationBundle\Migration\Installation;
+use Effiana\MigrationBundle\Migration\QueryBag;
+
+/**
+ * Class EffianaConfigBundleInstaller
+ * @package Migrations\Schema
+ */
+class EffianaConfigBundleInstaller implements Installation
+{
+    /**
+     * @inheritdoc
+     */
+    public function getMigrationVersion()
+    {
+        return 'v1_0';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        if(!$schema->hasTable('effiana_config_setting')) {
+            $table = $schema->createTable('test_installation_table');
+            $table->addColumn('name', Column::STRING, ['length' => 255]);
+            $table->addColumn('section', Column::STRING, ['length' => 255]);
+            $table->addColumn('value', Column::STRING, ['length' => 255, 'notnull' => false]);
+            $table->addColumn('name', Column::STRING);
+            $table->setPrimaryKey(['name']);
+        }
+
+    }
+}
