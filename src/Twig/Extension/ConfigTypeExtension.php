d60035c5087c9c0928bdd8e8a67b13d43d80f208
Dominik Łabudziński <dominik.labudzinski@brandoriented.pl>
2019-01-17 10:48:26 +0100

    Initial

---
+++ src/Twig/Extension/ConfigTypeExtension.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of the Effiana package.
+ *
+ * (c) Effiana, LTD
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Dominik Labudzinski <dominik@labudzinski.com>
+ */
+declare(strict_types=0);
+
+namespace Effiana\ConfigBundle\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * Class ConfigTypeExtension
+ * @package Effiana\ConfigBundle\Twig\Extension
+ */
+class ConfigTypeExtension extends AbstractExtension
+{
+    /**
+     * @return array|\Twig_Function[]
+     */
+    public function getFunctions()
+    {
+        return [
+            new TwigFilter('configType', [$this, 'configType']),
+        ];
+    }
+
+    /**
+     * @param string $value
+     * @param string $type
+     * @return int|string
+     */
+    public function configType(string $value, string $type)
+    {
+        settype($value, $type);
+
+        switch($type) {
+            case 'boolean':
+            case 'bool':
+                return ($value === true)?'On':'Off';
+                break;
+            case 'int':
+            case 'integer':
+                return (int)$value;
+                break;
+            case 'string':
+            default:
+                return $value;
+                break;
+        }
+    }
+}
